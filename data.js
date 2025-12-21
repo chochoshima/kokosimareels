ab6d5cf411bb78a44f7919824f3d4fea9796b9af
chochoshima <kokopujiyanto@gmail.com>
2025-12-21 08:08:38 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+const PROMPT_DATABASE = {
+    "ai-001": {
+        title: "RIDE",
+        videoUrl: "https://youtube.com/shorts/HAlAwqD8jGE", // Ganti dengan link asli
+        text: `A relaxed yet high-octane cinematic shot...`
+    },
+    "ai-002": {
+        title: "How To Train Your Dragon",
+        videoUrl: "https://youtube.com/shorts/VAF2idTiAe0",
+        text: `Ultra-realistic cinematic scene...`
+    },
+    "ai-003": {
+        title: "PINK RANGER",
+        videoUrl: "https://youtube.com/shorts/78uT5tR90sA",
+        text: `Using the provided photo reference...`
+    },
+    "ai-004": {
+        title: "BRAIN PROCESSOR PC",
+        videoUrl: "https://youtube.com/shorts/ftPcVhgqJvI",
+        text: `Ultra-realistic cinematic macro...`
+    }
+};
