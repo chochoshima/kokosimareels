8e9a792d6ade5627464d458879e2af19f62b6ce6
chochoshima <kokopujiyanto@gmail.com>
2025-12-22 22:56:56 +0700

    Update robots.txt.js

--- functions/robots.txt.js
+++ functions/robots.txt.js
@@ -1,10 +1,8 @@
 export async function onRequestGet() {
-  const lines = [
-    "User-agent: *",
-    "Allow: /",
-    "Sitemap: https://kokosimareels.pages.dev/sitemap.xml",
-  ];
-  return new Response(lines.join("\n"), {
-    headers: { "Content-Type": "text/plain" },
-  });
+  return new Response(
+    `User-agent: *
+Allow: /
+Sitemap: https://kokosimareels.pages.dev/sitemap.xml`,
+    { headers: { "Content-Type": "text/plain" } }
+  );
 }
