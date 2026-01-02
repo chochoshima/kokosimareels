2b0728a773cd846578a92292ff950e1668415052
chochoshima <kokopujiyanto@gmail.com>
2026-01-02 23:54:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,19 @@
-const CACHE_NAME = 'kokosima-v2';
-const assets = [
-  '/',
-  '/style.css',
-  '/data.js',
-  '/manifest.json'
-];
-
-// Install
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
-  self.skipWaiting();
-});
-
-// Activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// Fetch
 self.addEventListener('fetch', event => {
+  const req = event.request;
+
+  // Navigasi halaman (HTML)
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req, { redirect: 'follow' })
+        .catch(() => caches.match('/'))
+    );
+    return;
+  }
+
+  // Asset statis
   event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request, { redirect: 'follow' });
+    caches.match(req).then(res => {
+      return res || fetch(req, { redirect: 'follow' });
     })
   );
 });
